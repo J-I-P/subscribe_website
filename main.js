4ebbc12a6b6d0b25cb0af6221ba8f67373681191
Yi-Ping Jiang <apple80177@gmail.com>
2024-09-08 23:47:14 +0800

    feat: subscribe page and login page

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
+/*
+ * @Author: Yi-Ping Jiang apple80177@gmail.com
+ * @Date: 2024-09-08 19:29:58
+ * @LastEditors: Yi-Ping Jiang apple80177@gmail.com
+ * @LastEditTime: 2024-09-08 20:35:05
+ * @FilePath: /project/subscribe_website/main.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import "./assets/scss/all.scss";
 import "bootstrap";
-import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
+// import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
 
-console.log("Hello world!");
+// console.log("Hello world!");
 
-const swiper = new Swiper(".project-swiper", {
-  slidesPerView: 1,
-  centeredSlides: true,
-  spaceBetween: 0,
-});
+// const swiper = new Swiper(".project-swiper", {
+//   slidesPerView: 1,
+//   centeredSlides: true,
+//   spaceBetween: 0,
+// });
